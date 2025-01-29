fc5946f2a6ca4ca0708070e8c2dc476135ba61c6
vsal19 <vsal.ilham@gmail.com>
2025-01-29 23:22:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <h1 >Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+        </header>
+
+        <p>search</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
