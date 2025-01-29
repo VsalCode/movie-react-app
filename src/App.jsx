997616ff54383928ca5a0a31c70cfaf411752e73
vsal19 <vsal.ilham@gmail.com>
2025-01-30 03:30:22 +0700

    hero-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Search from "./components/search.jsx"; 
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern"></div>
 
       <div className="wrapper">
         <header>
-          <h1 >Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+          <img src="../public/hero-img.png" alt="Hero Image" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
         </header>
 
-        <p>search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> 
+        <h1>{searchTerm}</h1>
       </div>
     </main>
   );
-}
+};
 
 export default App;
